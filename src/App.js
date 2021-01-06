40a4d43373fce4bb9e5a1c8c8a992d9bbc4c6c36
flo20 <59807384+flo20@users.noreply.github.com>
2021-01-06 12:30:39 +0200

    Get current posts on pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Posts from "./components/Posts";
 
 const App = () => {
   const [posts, setPosts] = useState([]);
+  console.log(posts);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
@@ -13,12 +15,24 @@ const App = () => {
       const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
       setPosts(res.data);
       setLoading(false);
-        console.log(res.data);
-    }
-    fetchPosts()
-  
-  },[])
-  return <div>Hi</div>;
+      //console.log(res.data);
+    };
+    fetchPosts();
+  }, []);
+
+//Get current posts
+  const indexOfLastPost = currentPage * postsPerPage; //1 x 10
+  const indexOfFirstPost = indexOfLastPost - postsPerPage; // 10 - 10
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">Blog Page</h1>
+      <Posts posts={currentPosts} loading={loading}/>
+    </div>
+  );
 };
 
 export default App;
